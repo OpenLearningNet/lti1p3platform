67295d7e1c22065e20c0dfa471ccc994affff3fa
Jun Tu <jun@openlearning.com>
2023-03-07 21:09:24 +1100

    add method to generate template data

--- lti1p3platform/framework/django/message_launch.py
+++ lti1p3platform/framework/django/message_launch.py
@@ -4,14 +4,9 @@ from lti1p3platform.message_launch import MessageLaunchAbstract
 class DjangoLTI1P3MessageLaunch(MessageLaunchAbstract):
     def get_preflight_response(self) -> dict:
         return self._request.GET or self._request.POST
+
+    def template_data(self, launch_data):
+        raise NotImplementedError
     
-    def set_launch_url(self, launch_data, launch_url_name):
-        launch_data[launch_url_name] = self._registration.get_launch_url()
-
     def render_launch_form(self, launch_data, template, launch_url_name=None):
-        if launch_url_name:
-            self.set_launch_url(launch_data, launch_url_name)
-        else:
-            self.set_launch_url(launch_data, 'tool_launch_url')
-
-        return render(self._request, template, launch_data)
+        return render(self._request, template, self.template_data(launch_data))
